a298aad1f623629d38c0063e8b4b22a68f355071
Julczan <julek111222h@gmail.com>
2025-02-12 18:58:10 +0100

    creating multiple divs

--- javascript.js
+++ javascript.js
@@ -1,1 +1,7 @@
-console.log("dupa");
+const container = document.querySelector("#container");
+
+for (let i = 0; i < 16; i++) {
+  const div = document.createElement("div");
+  div.classList.add("square");
+  container.appendChild(div);
+}
