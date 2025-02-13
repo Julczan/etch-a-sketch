1550c0795366fa1486ba37d4bde9212d6a0a7636
Julczan <julek111222h@gmail.com>
2025-02-13 21:59:41 +0100

    fix: divs are being added

--- javascript.js
+++ javascript.js
@@ -1,10 +1,29 @@
 const container = document.querySelector("#container");
 
-for (let i = 0; i < 16; i++) {
+// let gridSize = prompt("Enter the number of elements");
+
+const button = document.querySelector("button");
+
+let gridSize = 4;
+
+for (let i = 0; i < gridSize * gridSize; i++) {
   const div = document.createElement("div");
   div.classList.add("square");
   container.appendChild(div);
   div.addEventListener("mouseover", () => {
-    div.style.backgroundColor = "blue";
+    div.style.backgroundColor = "red";
   });
 }
+
+button.addEventListener("click", () => {
+  gridSize = prompt("Enter the number of elements in a row");
+
+  for (let i = 0; i < gridSize; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    container.appendChild(div);
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = "red";
+    });
+  }
+});
