0f23011a6051200a6848adba678c9c774e67c8e4
Julczan <julek111222h@gmail.com>
2025-02-13 20:26:23 +0100

    changing background color on hover

--- javascript.js
+++ javascript.js
@@ -4,4 +4,7 @@ for (let i = 0; i < 16; i++) {
   const div = document.createElement("div");
   div.classList.add("square");
   container.appendChild(div);
+  div.addEventListener("mouseover", () => {
+    div.style.backgroundColor = "blue";
+  });
 }
