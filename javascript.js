b0f67dce37f5a151374c1394c2b494bc7c20e3d3
Julczan <julek111222h@gmail.com>
2025-02-14 16:28:21 +0100

    fixing the grid

--- javascript.js
+++ javascript.js
@@ -2,28 +2,14 @@ const container = document.querySelector("#container");
 
 // let gridSize = prompt("Enter the number of elements");
 
-const button = document.querySelector("button");
-
-let gridSize = 4;
-
-for (let i = 0; i < gridSize * gridSize; i++) {
-  const div = document.createElement("div");
-  div.classList.add("square");
-  container.appendChild(div);
-  div.addEventListener("mouseover", () => {
-    div.style.backgroundColor = "red";
-  });
-}
-
-button.addEventListener("click", () => {
-  gridSize = prompt("Enter the number of elements in a row");
-
-  for (let i = 0; i < gridSize; i++) {
-    const div = document.createElement("div");
-    div.classList.add("square");
-    container.appendChild(div);
-    div.addEventListener("mouseover", () => {
-      div.style.backgroundColor = "red";
-    });
+for (let i = 0; i < 4; i++) {
+  const row = document.createElement("div");
+  row.setAttribute("id", "row");
+  for (let i = 0; i < 4; i++) {
+    const column = document.createElement("div");
+    column.setAttribute("id", "column");
+    row.appendChild(column);
   }
-});
+
+  container.appendChild(row);
+}
