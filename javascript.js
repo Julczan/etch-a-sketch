1dd562fc55491ccf550ee8bc11fcadd0ec56670c
Julczan <julek111222h@gmail.com>
2025-02-18 22:58:15 +0100

    todo: delete all the elements

--- javascript.js
+++ javascript.js
@@ -1,21 +1,33 @@
 const container = document.querySelector("#container");
 
-// let gridSize = prompt("Enter the number of elements");
-
-let rowSize = 4;
-
-console.log(rowSize ** 2);
-
-let calculatedWidth = 100 / rowSize;
-console.log(calculatedWidth);
-
-for (let i = 0; i < rowSize ** 2; i++) {
-  const square = document.createElement("div");
-  square.setAttribute("id", "square");
-  square.style["width"] = calculatedWidth + "%";
-
-  container.appendChild(square);
+function changeGridSize() {
+  let rowSize = prompt("Enter the number of elements");
+  createGrid(rowSize);
 }
+
+function createGrid(rowSize) {
+  let calculatedWidth = 100 / rowSize;
+
+  for (let i = 0; i < rowSize ** 2; i++) {
+    const square = document.createElement("div");
+    square.setAttribute("id", "square");
+    square.style["width"] = calculatedWidth + "%";
+
+    container.appendChild(square);
+
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "red";
+    });
+  }
+}
+
+let button = document.querySelector("button");
+button.addEventListener("click", () => {
+  square.remove();
+});
+createGrid(4);
 
 //Each element has to be a square
 //while resizing squares have to get smaller
+//removeChild doesn't work on the square
+// how to remove multiple elements from the dom
